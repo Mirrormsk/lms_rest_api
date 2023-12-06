9ba8a6d70aee03931fcfec5116b1287f5668a404
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-06 11:55:11 +0300

    создано приложение lessons с моделями Course и Lesson

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from users.models import NULLABLE
 
-# Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='lessons', verbose_name='превью', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+
+    title = models.CharField(max_length=255, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='lessons', verbose_name='превью', **NULLABLE)
+    video_url = models.URLField(**NULLABLE, verbose_name='ссылка на видео')
+    course = models.ManyToManyField(Course)
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+
+    def __str__(self):
+        return self.title
+
+
