5ab631d8c2817d7a3968873a9dc64330afbfb868
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-06 11:42:44 +0300

    поле city в модели User сделано необязательным

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     username = None
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     email = models.EmailField(unique=True, verbose_name='почта')
-    city = models.CharField(max_length=100, verbose_name='город')
+    city = models.CharField(max_length=100, verbose_name='город', **NULLABLE)
     uid = models.UUIDField(default=uuid.uuid4, editable=False)
     image = models.ImageField(upload_to='users', verbose_name='аватар', **NULLABLE)
 
