315e86979e2c52eaff5012eb51b6e49f70488ef5
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-08 15:21:23 +0300

    написаны контроллер для вывода списка платежей и прописан url

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
 
-# Create your views here.
+
+class PaymentListAPIView(generics.ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
