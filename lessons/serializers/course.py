e0dfdbcb988a598164301b02f6fb050225119715
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-08 13:58:19 +0300

    для курса написан вывод количества уроков

--- lessons/serializers/course.py
+++ lessons/serializers/course.py
@@ -5,6 +5,8 @@ from lessons.models import Course
 
 class CourseSerializer(serializers.ModelSerializer):
 
+    lessons_count = serializers.IntegerField(read_only=True, source='lessons.count')
+
     class Meta:
         model = Course
         fields = '__all__'
