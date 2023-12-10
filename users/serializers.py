6a1420806244d9b2145eb8d0ace4f41f6291a1f0
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-10 21:15:11 +0300

    для модели пользователя сделан вывод информации о платежах

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from users.models import User
+from payments.serializers import PaymentSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    payments = PaymentSerializer(many=True, read_only=True, source='payment_set')
 
     class Meta:
         model = User
