ec6e03131ea507078da22431432bf5f37c9d6062
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-11 11:58:23 +0300

    поле course в сериализаторе PaymentSerializer выводит title вместо id

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from lessons.models import Lesson
+from lessons.models import Lesson, Course
 from payments.models import Payment
 from users.models import User
 
@@ -9,9 +9,9 @@ class PaymentSerializer(serializers.ModelSerializer):
 
     user = serializers.SlugRelatedField("email", queryset=User.objects.all())
     lesson = serializers.SlugRelatedField("title", queryset=Lesson.objects.all())
-    method = serializers.CharField(source='get_method_display')
+    course = serializers.SlugRelatedField("title", queryset=Course.objects.all())
+    method = serializers.CharField(source="get_method_display")
 
     class Meta:
         model = Payment
         fields = "__all__"
-
