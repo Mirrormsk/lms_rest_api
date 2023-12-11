a23cc50cbc1c71a467e67908110b2dbf7253fd12
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-11 18:25:00 +0300

    добавлены url для jwt

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from users.apps import UsersConfig
 from users.views import UserViewSet
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
+router.register(r"users", UserViewSet, basename="users")
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + router.urls
