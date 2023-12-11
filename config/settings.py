c789848c336e6d11e6f619f293c8ad97cf9b1133
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-11 17:48:44 +0300

    настройка djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,6 @@
 import os
+from datetime import timedelta
+
 import environ
 from pathlib import Path
 
@@ -106,6 +108,11 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 
