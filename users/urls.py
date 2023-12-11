ba58a09bf02e0ba6a3896722003110006b3fc62e
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-11 18:26:00 +0300

    убран префикс api/ в url для jwt

--- users/urls.py
+++ users/urls.py
@@ -14,6 +14,6 @@ router.register(r"users", UserViewSet, basename="users")
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ] + router.urls
