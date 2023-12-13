95976f581d5573fd7f3fa7bd9c027263bb4d173e
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-13 17:41:12 +0300

    Все контроллеры закрыты для неавторизованных юзеров

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
 from users.serializers import UserSerializer
@@ -7,3 +8,4 @@ from users.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
