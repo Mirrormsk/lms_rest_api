1db2df50c1caf9ffc63e6b05bf2efc958547a26f
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-13 17:41:04 +0300

    Все контроллеры закрыты для неавторизованных юзеров

--- lessons/views/lesson.py
+++ lessons/views/lesson.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from lessons.models import Lesson
 from lessons.serializers.lesson import LessonSerializer
@@ -7,23 +8,28 @@ from lessons.serializers.lesson import LessonSerializer
 class LessonListView(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonCreateView(generics.CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonRetrieveView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonUpdateView(generics.UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class LessonDeleteView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
