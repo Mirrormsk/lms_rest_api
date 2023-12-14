1d8312d72344adfaaa589908870352a851334612
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-14 10:17:23 +0300

    Модератор может видеть любые уроки, а пользователь - только свои.

--- lessons/views/lesson.py
+++ lessons/views/lesson.py
@@ -4,12 +4,24 @@ from rest_framework.permissions import IsAuthenticated
 from lessons.models import Lesson
 from lessons.permissions import IsModerator, IsOwner
 from lessons.serializers.lesson import LessonSerializer
+from lessons.services import user_in_group
 
 
 class LessonListView(generics.ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if not user.is_authenticated:
+            return Lesson.objects.none()
+
+        if user_in_group(user, group_name="moderators"):
+            return Lesson.objects.all()
+
+        return Lesson.objects.filter(owner=user)
 
 
 class LessonCreateView(generics.CreateAPIView):
