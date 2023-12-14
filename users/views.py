4d2dd124d768bafc6eaea06716b2ddd0210d7fbf
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-14 11:16:31 +0300

    При просмотре списка юзеров или чужого профиля используется UserPublicSerializer, в остальных случаях - UserSerializer

--- users/views.py
+++ users/views.py
@@ -1,11 +1,31 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
-from users.serializers import UserSerializer
+from users.permissions import IsProfileOwnerOrReadOnly
+from users.serializers import UserSerializer, UserPublicSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsProfileOwnerOrReadOnly]
+
+    def get_serializer_class(self):
+        user = self.request.user
+
+        if self.action == 'retrieve':
+            instance = self.get_object()
+            if instance == user:
+                return UserSerializer
+            return UserPublicSerializer
+
+        elif self.action == 'list':
+            return UserPublicSerializer
+
+        elif self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return UserSerializer
+
+
+
+
