0a957d3b46cf9af743d87986c5ecd205e0dc2da0
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-14 17:35:36 +0300

    Добавлена пагинация в вывод списка уроков. Также оптимизированы запосы к БД

--- lessons/views/lesson.py
+++ lessons/views/lesson.py
@@ -2,15 +2,17 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
 from lessons.models import Lesson
+from lessons.paginators import LessonPagination
 from lessons.permissions import IsModerator, IsOwner
 from lessons.serializers.lesson import LessonSerializer
 from lessons.services import user_in_group
 
 
 class LessonListView(generics.ListAPIView):
-    queryset = Lesson.objects.all()
+
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = LessonPagination
 
     def get_queryset(self):
         user = self.request.user
@@ -19,7 +21,7 @@ class LessonListView(generics.ListAPIView):
             return Lesson.objects.none()
 
         if user_in_group(user, group_name="moderators"):
-            return Lesson.objects.all()
+            return Lesson.objects.prefetch_related("course_set").all()
 
         return Lesson.objects.filter(owner=user)
 
