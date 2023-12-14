698575788e35816450ea047e222943d6f58d9780
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-14 10:14:51 +0300

    Добавлено автоматическое добавление поля owner

--- lessons/views/lesson.py
+++ lessons/views/lesson.py
@@ -17,6 +17,9 @@ class LessonCreateView(generics.CreateAPIView):
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated, ~IsModerator]
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
 
 class LessonRetrieveView(generics.RetrieveAPIView):
     queryset = Lesson.objects.all()
