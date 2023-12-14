1b0ee7d7507a8caedb92f8a78022390c0054c8b8
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-14 09:15:46 +0300

    Добавлено поле owner к моделям Lesson и Course

--- lessons/models.py
+++ lessons/models.py
@@ -4,29 +4,34 @@ from users.models import NULLABLE
 
 class Lesson(models.Model):
 
-    title = models.CharField(max_length=255, verbose_name='название')
-    description = models.TextField(verbose_name='описание')
-    preview = models.ImageField(upload_to='lessons', verbose_name='превью', **NULLABLE)
-    video_url = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+    title = models.CharField(max_length=255, verbose_name="название")
+    description = models.TextField(verbose_name="описание")
+    preview = models.ImageField(upload_to="lessons", verbose_name="превью", **NULLABLE)
+    video_url = models.URLField(verbose_name="ссылка на видео", **NULLABLE)
+    owner = models.ForeignKey(
+        "users.User", on_delete=models.SET_NULL, **NULLABLE, verbose_name="Создал"
+    )
 
     class Meta:
-        verbose_name = 'урок'
-        verbose_name_plural = 'уроки'
+        verbose_name = "урок"
+        verbose_name_plural = "уроки"
 
     def __str__(self):
         return self.title
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=255, verbose_name='название')
-    description = models.TextField(verbose_name='описание')
-    preview = models.ImageField(upload_to='lessons', verbose_name='превью', **NULLABLE)
+    title = models.CharField(max_length=255, verbose_name="название")
+    description = models.TextField(verbose_name="описание")
+    preview = models.ImageField(upload_to="lessons", verbose_name="превью", **NULLABLE)
     lessons = models.ManyToManyField(Lesson)
+    owner = models.ForeignKey(
+        "users.User", on_delete=models.SET_NULL, **NULLABLE, verbose_name="Создал"
+    )
 
     class Meta:
-        verbose_name = 'курс'
-        verbose_name_plural = 'курсы'
+        verbose_name = "курс"
+        verbose_name_plural = "курсы"
 
     def __str__(self):
         return self.title
-
