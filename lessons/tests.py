7342a93f131a69393cc76e1dff1fc5f34201d04d
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-18 14:16:07 +0300

    добавлен тест на удаление урока

--- lessons/tests.py
+++ lessons/tests.py
@@ -12,6 +12,9 @@ class LessonsTestCase(APITransactionTestCase):
     def setUp(self):
         self.lesson_create_url = reverse("lessons:lesson-create")
         self.user1 = User.objects.create(email="test@test.com", password="Password1234")
+        self.user2 = User.objects.create(
+            email="test2@test.com", password="Password1234"
+        )
         self.lesson = Lesson.objects.create(
             title="Lesson 1", description="Lesson 1", owner=self.user1
         )
@@ -23,10 +26,8 @@ class LessonsTestCase(APITransactionTestCase):
         # Очистит базу данных после теста и сбросьте счетчик id
         Lesson.objects.all().delete()
 
-
-    def test_get_lessons_list(self):
+    def test_1_get_lessons_list(self):
         """Тест вывода списка уроков"""
-        print("Вызов test_get_lessons_list")
         self.client.force_authenticate(user=self.user1)
         response = self.client.get("/lessons/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -50,9 +51,41 @@ class LessonsTestCase(APITransactionTestCase):
             },
         )
 
-    def test_create_lesson(self):
+    def test_2_create_lesson(self):
         self.client.force_authenticate(user=self.user1)
         new_lesson_data = {"title": "Lesson New", "description": "Lesson New"}
         response = self.client.post(self.lesson_create_url, new_lesson_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertIn(new_lesson_data, response.json())
+
+    def test_3_delete_lesson(self):
+        self.client.force_authenticate(user=self.user1)
+
+        # Creating new lesson
+        new_lesson_data = {"title": "Lesson New", "description": "Lesson New"}
+        response = self.client.post(self.lesson_create_url, new_lesson_data)
+        new_lesson_id = response.data["id"]
+
+        # Try to get by owner
+        try_to_get_created_lesson = self.client.get(
+            reverse("lessons:lesson-detail", args=[new_lesson_id])
+        )
+        self.assertEqual(try_to_get_created_lesson.status_code, status.HTTP_200_OK)
+
+        # The user cannot delete someone else's own lesson
+        self.client.force_authenticate(user=self.user2)
+        delete_response = self.client.delete(
+            reverse("lessons:lesson-delete", args=[new_lesson_id])
+        )
+        self.assertEqual(delete_response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # Owner can delete lesson
+        self.client.force_authenticate(user=self.user1)
+        self.client.delete(reverse("lessons:lesson-delete", args=[new_lesson_id]))
+
+        # Check that lesson was deleted
+        try_to_get_deleted_lesson = self.client.get(
+            reverse("lessons:lesson-detail", args=[new_lesson_id])
+        )
+        self.assertEqual(
+            try_to_get_deleted_lesson.status_code, status.HTTP_404_NOT_FOUND
+        )
