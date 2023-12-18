19eaddb5df766b596c64aa86485622807d8dbaa2
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-18 17:46:35 +0300

    добавлен эндпоинт subscription/<int:pk>/delete/

--- lessons/views/subscription.py
+++ lessons/views/subscription.py
@@ -1,5 +1,6 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from lessons.models import Subscription
 from lessons.serializers.subscription import SubscriptionSerializer
@@ -11,3 +12,13 @@ class SubscriptionCreateAPIView(generics.CreateAPIView):
     serializer_class = SubscriptionSerializer
 
 
+class SubscriptionDeleteAPIView(generics.DestroyAPIView):
+    queryset = Subscription.objects.all()
+    permission_classes = [IsAuthenticated]
+    serializer_class = SubscriptionSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance: Subscription = self.get_object()
+        instance.is_active = False
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
