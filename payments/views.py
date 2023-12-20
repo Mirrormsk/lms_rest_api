a1fb434b33f7be6cc214078d496c5ecbdd68703d
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-20 09:34:18 +0300

    Добавлен контроллер для создания платежа

--- payments/views.py
+++ payments/views.py
@@ -1,9 +1,13 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.filters import OrderingFilter
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
 
 from payments.models import Payment
-from payments.serializers import PaymentSerializer
+from payments.serializers import PaymentSerializer, PaymentCreateSerializer
+from payments.services import StripeApiClient, PaymentService
 
 
 class PaymentListAPIView(generics.ListAPIView):
@@ -14,4 +18,19 @@ class PaymentListAPIView(generics.ListAPIView):
     filterset_fields = ["course", "lesson", "method"]
     search_fields = ["course", "lesson__title", "method"]
     ordering_fields = ["created_at"]
+
+
+class PaymentCreateView(APIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentCreateSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            payment = serializer.save()
+            response = PaymentService.process_payment(payment)
+            return response
+        else:
+            return Response({"status": "failed", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
 
