3fcfa29a70cd351a7dd554784b82080281804a75
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-20 15:49:35 +0300

    добавлена модель платежной сессии

--- payments/models.py
+++ payments/models.py
@@ -19,10 +19,24 @@ class Payment(models.Model):
     amount = models.PositiveIntegerField(verbose_name='сумма')
     method = models.CharField(max_length=30, choices=METHODS, verbose_name='метод')
     is_paid = models.BooleanField(default=False, verbose_name='оплачен')
-
     class Meta:
         verbose_name = 'платеж'
         verbose_name_plural = 'платежи'
 
     def __str__(self):
         return f"{self.created_at}: {self.user.email}, {self.amount}"
+
+
+class PaymentSession(models.Model):
+    payment = models.OneToOneField(Payment, on_delete=models.CASCADE, related_name='session')
+    session_id = models.CharField(max_length=255, unique=True, verbose_name='session id')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создана')
+
+    class Meta:
+        verbose_name = 'платежная сессия'
+        verbose_name_plural = 'платежные сессии'
+        unique_together = ('payment', 'session_id')
+        ordering = ('-created_at', "payment")
+
+    def __str__(self):
+        return f"{self.session_id}"
