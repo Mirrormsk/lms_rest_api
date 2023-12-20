4d9c55f8a5140a6ada9dd4cd9e49d2df31eff267
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-20 15:49:23 +0300

    добавлен контроллер PaymentCheckStatusAPIView для проверки статуса платежа

--- payments/views.py
+++ payments/views.py
@@ -1,4 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, status
 from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
@@ -6,7 +8,11 @@ from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 
 from payments.models import Payment
-from payments.serializers import PaymentSerializer, PaymentCreateSerializer
+from payments.serializers import (
+    PaymentSerializer,
+    PaymentCreateSerializer,
+    PaymentCheckStatusSerializer,
+)
 from payments.services import StripeApiClient, PaymentService
 
 
@@ -24,6 +30,20 @@ class PaymentCreateView(APIView):
     queryset = Payment.objects.all()
     serializer_class = PaymentCreateSerializer
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "user": openapi.Schema(type=openapi.TYPE_INTEGER, description="User ID"),
+                "lesson": openapi.Schema(type=openapi.TYPE_INTEGER, description="Lesson ID"),
+                "course": openapi.Schema(type=openapi.TYPE_INTEGER, description="Course ID"),
+                "method": openapi.Schema(type=openapi.TYPE_STRING, enum=["cash", "account"], description="Payment method"),
+                "amount": openapi.Schema(type=openapi.TYPE_INTEGER, description="Amount"),
+            },
+            required=["user",  "method", "amount"],
+
+            )
+    )
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -31,6 +51,35 @@ class PaymentCreateView(APIView):
             response = PaymentService.process_payment(payment)
             return response
         else:
-            return Response({"status": "failed", "errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"status": "failed", "errors": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
+class PaymentCheckStatusAPIView(APIView):
+    """
+    Check payment status (by id).
+    """
+
+    queryset = Payment.objects.all()
+    serializer_class = PaymentCheckStatusSerializer
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'payment_id': openapi.Schema(type=openapi.TYPE_INTEGER, description='Payment id')
+            }
+        ),
+        responses={
+            status.HTTP_200_OK: openapi.Response(description="Payment info", schema=PaymentSerializer)
+        }
+    )
+    def post(self, request, *args, **kwargs):
+        payment_id = request.data.get("payment_id")
+        try:
+            payment = Payment.objects.get(pk=payment_id)
+        except Payment.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        return PaymentService.update_payment_status(payment)
