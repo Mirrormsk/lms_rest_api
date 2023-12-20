24a4e67655d1ccc48bce3685c17df5e186ac6f1a
Maxim Donchuk <m.donchuk@gmail.com>
2023-12-20 15:49:07 +0300

    добавлен URL для проверки статуса платежа

--- payments/urls.py
+++ payments/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from payments.apps import PaymentsConfig
-from payments.views import PaymentListAPIView, PaymentCreateView
+from payments.views import PaymentListAPIView, PaymentCreateView, PaymentCheckStatusAPIView
 
 app_name = PaymentsConfig.name
 
 urlpatterns = [
     path('', PaymentListAPIView.as_view(), name='payment-list'),
     path('create/', PaymentCreateView.as_view(), name='payment-create'),
-
+    path('check/', PaymentCheckStatusAPIView.as_view(), name='payment-check'),
 ]
